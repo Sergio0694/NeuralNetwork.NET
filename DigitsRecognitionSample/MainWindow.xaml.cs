d9d6856c2c0d94c85bccaf31512d71ea76705d62
Sergio Pedri <Sergio0694@live.com>
2017-04-02 16:15:27 +0200

    Added ability to pick an image, convert to grayscale and save it

--- DigitsRecognitionSample/MainWindow.xaml.cs
+++ DigitsRecognitionSample/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
 
 namespace DigitsRecognitionSample
 {
@@ -55,5 +58,61 @@ namespace DigitsRecognitionSample
         }
 
         #endregion
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog picker = new OpenFileDialog
+            {
+                DefaultExt = ".png",
+                Filter = "PNG image (.png)|*.png"
+            };
+            bool? result = picker.ShowDialog();
+            if (result.HasValue && result.Value)
+            {
+                Bitmap 
+                    image = new Bitmap(picker.FileName),
+                    grayscale = ToGrayscale(image);
+                SaveFileDialog saver = new SaveFileDialog
+                {
+                    DefaultExt = ".png",
+                    Filter = "PNG image (.png)|*.png"
+                };
+                result = saver.ShowDialog();
+                if (result.HasValue && result.Value)
+                {
+                    grayscale.Save(saver.FileName);
+                }
+            }
+        }
+
+        public static Bitmap ToGrayscale(Bitmap original)
+        {
+            // Create a blank bitmap the same size as original
+            Bitmap newBitmap = new Bitmap(original.Width, original.Height);
+
+            // Get a graphics object from the new image
+            using (Graphics g = Graphics.FromImage(newBitmap))
+            {
+                // Create the grayscale ColorMatrix
+                ColorMatrix colorMatrix = new ColorMatrix(
+                new float[][]
+                {
+                    new float[] { 0.3f, 0.3f, 0.3f, 0, 0 },
+                    new float[] { 0.59f, 0.59f, 0.59f, 0, 0 },
+                    new float[] { 0.11f, 0.11f, 0.11f, 0, 0 },
+                    new float[] { 0, 0, 0, 1, 0 },
+                    new float[] { 0, 0, 0, 0, 1 }
+                });
+
+                // Create the image attributes and set the color matrix
+                ImageAttributes attributes = new ImageAttributes();
+                attributes.SetColorMatrix(colorMatrix);
+
+                // Draw the original image on the new image using the grayscale color matrix
+                g.DrawImage(original, new System.Drawing.Rectangle(0, 0, original.Width, original.Height),
+                    0, 0, original.Width, original.Height, GraphicsUnit.Pixel, attributes);
+                return newBitmap;
+            }
+        }
     }
 }
