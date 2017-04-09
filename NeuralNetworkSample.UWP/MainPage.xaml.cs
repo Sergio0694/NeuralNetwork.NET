c87e95def78087d28e5df8ce521ac84e9c71f5ce
Sergio Pedri <Sergio0694@live.com>
2017-04-10 01:06:35 +0200

    Namespace adjusted in UWP sample project

--- NeuralNetworkSample.UWP/MainPage.xaml.cs
+++ NeuralNetworkSample.UWP/MainPage.xaml.cs
@@ -1,21 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+﻿using Windows.Foundation;
 using Windows.UI.Core;
 using Windows.UI.ViewManagement;
-using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
-namespace NeuralNetworkSample.UWP
+namespace NeuralNetworkSampleUWP
 {
     /// <summary>
     /// Pagina vuota che può essere usata autonomamente oppure per l'esplorazione all'interno di un frame.
