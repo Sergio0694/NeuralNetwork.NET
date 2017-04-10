e671c40675a8440a3ea3fc345feda6f60e2cd299
Sergio Pedri <Sergio0694@live.com>
2017-04-10 02:13:01 +0200

    Base genetic algorithm code moved to .NET Standard library (WIP)

---
+++ NeuralNetwork.NET/UnsupervisedLearning/Misc/GeneticAlgorithmBestNetworkChangedEventArgs.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using NeuralNetworkNET.Networks.PublicAPIs;
+
+namespace NeuralNetworkNET.UnsupervisedLearning.Misc
+{
+    /// <summary>
+    /// The argument for the BestSpeciesChanged event
+    /// </summary>
+    public sealed class GeneticAlgorithmBestNetworkChangedEventArgs : EventArgs
+    {
+        /// <summary>
+        /// Gets the generated neural network
+        /// </summary>
+        public INeuralNetwork Network { get; }
+
+        /// <summary>
+        /// Gets the fitness score obtained by the neural network
+        /// </summary>
+        public double Fitness { get; }
+
+        // Internal constructor
+        internal GeneticAlgorithmBestNetworkChangedEventArgs(INeuralNetwork network, double fitess)
+        {
+            Network = network;
+            Fitness = fitess;
+        }
+    }
+}
