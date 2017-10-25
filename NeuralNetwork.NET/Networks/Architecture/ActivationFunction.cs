34815dada735cd7943d6f103070011c61d10a2ce
Sergio Pedri <sergio0694@live.com>
2017-10-25 20:07:28 +0200

    Added an enum to indicate an activation function

---
+++ NeuralNetwork.NET/Networks/Architecture/ActivationFunction.cs
@@ -0,0 +1,15 @@
+﻿namespace NeuralNetworkNET.Networks.Architecture
+{
+    /// <summary>
+    /// Indicates an activation function to use in a neural network
+    /// </summary>
+    public enum ActivationFunction
+    {
+        Sigmoid,
+        Tanh,
+        ReLU,
+        LeakyReLU,
+        Softplus,
+        ELU
+    }
+}
