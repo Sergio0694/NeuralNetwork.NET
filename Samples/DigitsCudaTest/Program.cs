cda7bf50f3faeb6766f21894e3f8e70e74431e64
Sergio Pedri <Sergio0694@live.com>
2017-10-26 09:22:11 +0200

    Cuda sample app migrated

---
+++ Samples/DigitsCudaTest/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using NeuralNetworkNET.Cuda.APIs;
+using NeuralNetworkNET.SupervisedLearning.Misc;
+using SharedBenchmark;
+
+namespace DigitsCudaTest
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            NeuralNetworkGpuPreferences.ProcessingMode = ProcessingMode.Gpu;
+            await MnistTester.PerformBenchmarkAsync(LearningAlgorithmType.BoundedBFGSWithGradientDescentOnFirstConvergence, false, null, true, 784, 16, 16, 10);
+        }
+    }
+}
