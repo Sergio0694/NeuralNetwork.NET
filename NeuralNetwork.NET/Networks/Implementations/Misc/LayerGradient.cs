7eb07574b6bcfb2024b13f9100869385077127e3
Sergio Pedri <Sergio0694@live.com>
2017-11-10 10:07:38 +0100

    Added an extension to flatten a list of gradients

--- NeuralNetwork.NET/Networks/Implementations/Misc/LayerGradient.cs
+++ NeuralNetwork.NET/Networks/Implementations/Misc/LayerGradient.cs
@@ -1,4 +1,8 @@
-﻿using JetBrains.Annotations;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using JetBrains.Annotations;
+using NeuralNetworkNET.Helpers;
 
 namespace NeuralNetworkNET.Networks.Implementations.Misc
 {
@@ -21,6 +25,36 @@ namespace NeuralNetworkNET.Networks.Implementations.Misc
         {
             DJdw = dJdw;
             Djdb = dJdb;
+        }
+    }
+
+    /// <summary>
+    /// A small extension class for the <see cref="LayerGradient"/> structure
+    /// </summary>
+    internal static class LayerGradientExtensions
+    {
+        /// <summary>
+        /// Flattens a list of layered gradients into a single linear array
+        /// </summary>
+        /// <param name="gradient">The gradients to flatten</param>
+        [PublicAPI]
+        [Pure, NotNull]
+        [CollectionAccess(CollectionAccessType.Read)]
+        public static double[] Flatten([NotNull] this IReadOnlyList<LayerGradient> gradient)
+        {
+            if (gradient.Count == 0) return new double[0];
+            double[] result = new double[gradient.Sum(g => g.DJdw.Length + g.Djdb.Length)];
+            int position = 0;
+            foreach (LayerGradient layer in gradient)
+            {
+                int n = sizeof(double) * layer.DJdw.Length;
+                Buffer.BlockCopy(layer.DJdw, 0, result, position, n);
+                position += n;
+                n = sizeof(double) * layer.Djdb.Length;
+                Buffer.BlockCopy(layer.Djdb, 0, result, position, n);
+                position += n;
+            }
+            return result;
         }
     }
 }
