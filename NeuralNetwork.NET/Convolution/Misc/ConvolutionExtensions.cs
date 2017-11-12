c6049d515487651d580b6f44762079406c487dba
Sergio Pedri <sergio0694@live.com>
2017-11-13 00:46:15 +0100

    Variable size convolution base method added

--- NeuralNetwork.NET/Convolution/Misc/ConvolutionExtensions.cs
+++ NeuralNetwork.NET/Convolution/Misc/ConvolutionExtensions.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using JetBrains.Annotations;
+using NeuralNetworkNET.Exceptions;
 using NeuralNetworkNET.Helpers;
 
 namespace NeuralNetworkNET.Convolution.Misc
@@ -158,6 +162,106 @@ namespace NeuralNetworkNET.Convolution.Misc
                 x++;
             }
             return result;
+        }
+
+        /// <summary>
+        /// Performs a convolution operation on the source matrix, using the given kernels
+        /// </summary>
+        /// <param name="source">The source matrix, where each row is a sample in the dataset and each one contains a series of images in row-first order</param>
+        /// <param name="subdivision">The number of images in the data volume associated to each sample</param>
+        /// <param name="kernels">The list of convolution kernels to apply to each image</param>
+        /// <param name="mode">The desired convolution mode to use to process the input matrix</param>
+        /// <returns>A new matrix where each row contains the result of the convolutions for each original image for each sample</returns>
+        /// <exception cref="ArgumentException">The size of the matrix isn't valid, or the kernels list isn't valid</exception>
+        /// <exception cref="ArgumentOutOfRangeException">The size of the matrix doesn't match the expected values</exception>
+        [PublicAPI]
+        [Pure, NotNull]
+        [CollectionAccess(CollectionAccessType.Read)]
+        public static float[,] Convolute3x3([NotNull] this float[,] source, int subdivision, [NotNull] IReadOnlyList<float[,]> kernels, ConvolutionMode mode)
+        {
+            // Checks
+            if (source.Length == 0) throw new ArgumentException(nameof(source), "The source matrix can't be empty");
+            if (subdivision < 1) throw new ArgumentOutOfRangeException(nameof(subdivision), "The number of images per row can't be lower than 1");
+            if (kernels.Count == 0) throw new ArgumentException(nameof(kernels), "The kernels list can't be empty");
+            int
+                kh = kernels[0].GetLength(0),
+                kw = kernels[0].GetLength(1);
+            if (kh != kw) throw new ArgumentException(nameof(kernels), "The kernel must be a square matrix");
+            if (kh < 2) throw new ArgumentException(nameof(kernels), "The kernel must be at least 2x2");
+            if (kernels.Any(k => k.GetLength(0) != kh || k.GetLength(1) != kw))
+                throw new ArgumentException(nameof(kernels), "One of the input kernels doesn't have a valid size");
+
+            // Local parameters
+            int
+                h = source.GetLength(0),
+                w = source.GetLength(1),
+                klen = kernels.Count,
+                ksize = kh * kw,
+                imgSize = w % subdivision == 0 ? w / subdivision : throw new ArgumentException(nameof(source), "Invalid subdivision parameter for the input matrix"),
+                imgAxis = imgSize.IntegerSquare();  // Size of an edge of one of the inner images per sample
+            if (imgAxis * imgAxis != imgSize) throw new ArgumentOutOfRangeException(nameof(source), "The size of the input matrix isn't valid");
+            if (imgSize < ksize) throw new ArgumentOutOfRangeException("Each subdivided matrix must at least have the size of the kernels");
+            
+            // Valid convolution, result smaller than original
+            if (mode == ConvolutionMode.Valid)
+            {
+                int
+                inner = imgAxis - kh + 1,                                       // Size of each image edge after the convolution
+                convolutionOutputSize = inner * inner,                          // Size of each processed image
+                finalWidth = inner * inner * subdivision * klen,                // Final size of each sample row
+                iterationsPerSample = subdivision * klen;                       // Iterations for each dataset entry
+
+                // Process the whole data in a single step
+                float[,] result = new float[h, finalWidth];
+
+                // Convolution kernel
+                unsafe void Kernel(int index)
+                {
+                    // Calculate the current indexes
+                    int
+                        i = index / iterationsPerSample,            // Sample index
+                        i_mod = index % iterationsPerSample,
+                        j = i_mod / klen,                           // Subdivision index
+                        k = i_mod % klen;                           // Kernel index
+
+                    // Read and write sample and subdivision offsets
+                    int
+                        sourceOffset = i * w + j * imgSize,
+                        resultOffset = i * finalWidth + j * convolutionOutputSize;
+                    fixed (float* pk = kernels[k], psource = source, presult = result)
+                    {
+                        for (int x = 0; x < inner; x++)
+                        {
+                            int offset = resultOffset + x * inner;
+                            for (int y = 0; y < inner; y++)
+                            {
+                                // Process a single convolution of variable size
+                                float convolution = 0;
+                                for (int kx = 0; kx < kh; kx++)
+                                {
+                                    int
+                                        kOffset = kx * kh,
+                                        ksourceOffset = sourceOffset + (kx + x) * imgAxis + y;
+                                    for (int ky = 0; ky < kh; ky++)
+                                        convolution += pk[kOffset + ky] * psource[ksourceOffset + ky];
+                                }
+                                presult[offset + y] = convolution;
+                            }
+                        }
+                    }
+                }
+
+                // Convolute in parallel
+                Parallel.For(0, h * iterationsPerSample, Kernel).AssertCompleted();
+                return result;
+            }
+
+            // Full convolution
+            if (mode == ConvolutionMode.Full)
+            {
+                throw new NotImplementedException();
+            }
+            throw new ArgumentOutOfRangeException(nameof(mode), "Unsupported convolution mode");
         }
     }
 }
---
+++ NeuralNetwork.NET/Convolution/Misc/ConvolutionMode.cs
@@ -0,0 +1,18 @@
+﻿namespace NeuralNetworkNET.Convolution.Misc
+{
+    /// <summary>
+    /// Indicates the convolution mode to use when performing the convolution operation
+    /// </summary>
+    public enum ConvolutionMode
+    {
+        /// <summary>
+        /// Valid convolution, where the kernel only slides within the margins of the target matrix
+        /// </summary>
+        Valid,
+
+        /// <summary>
+        /// Full convolution, where the kernel slides from each edge of the target matrix, even if it partially exceeds the bounds of that matrix
+        /// </summary>
+        Full
+    }
+}
