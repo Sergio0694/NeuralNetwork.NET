aaa94610b3fc4c4a1eec9099010ba60f1ed8c805
Sergio Pedri <sergio0694@live.com>
2017-12-02 19:23:59 +0100

    Added new GPU methods to manually manage memory

--- NeuralNetwork.NET.Cuda/Helpers/GpuExtensions.cs
+++ NeuralNetwork.NET.Cuda/Helpers/GpuExtensions.cs
@@ -1,51 +1,94 @@
 ﻿using System;
-using System.Runtime.InteropServices;
 using Alea;
 using JetBrains.Annotations;
+using NeuralNetworkNET.Helpers;
+using NeuralNetworkNET.Structs;
 
 namespace NeuralNetworkNET.Cuda.Helpers
 {
     /// <summary>
     /// An extension class with some additions to the <see cref="Gpu"/> class
     /// </summary>
-    public static class GpuExtensions
+    internal static class GpuExtensions
     {
         /// <summary>
-        /// Gets the name of the current CUDA dll in use
+        /// Allocates a 2D memory area on device memory and copies the contents of the input 2D span
         /// </summary>
-#if LINUX
-        private const string CUDA_DLL_NAME = "libcuda";
+        /// <param name="gpu">The <see cref="Gpu"/> device to use</param>
+        /// <param name="source">The source <see cref="FloatSpan2D"/> with the data to copy</param>
+        [MustUseReturnValue, NotNull]
+        public static unsafe DeviceMemory2D<float> AllocateDevice([NotNull] this Gpu gpu, in FloatSpan2D source)
+        {
+            DeviceMemory2D<float> result_gpu = gpu.AllocateDevice<float>(source.Height, source.Width);
+            CUDAInterop.CUDA_MEMCPY2D_st* ptSt = stackalloc CUDAInterop.CUDA_MEMCPY2D_st[1];
+            ptSt[0] = new CUDAInterop.CUDA_MEMCPY2D_st
+            {
+                srcMemoryType = CUDAInterop.CUmemorytype_enum.CU_MEMORYTYPE_HOST,
+                srcHost = source.Ptr,
+                srcPitch = new IntPtr(sizeof(float) * source.Width),
+                dstMemoryType = CUDAInterop.CUmemorytype_enum.CU_MEMORYTYPE_DEVICE,
+                dstDevice = result_gpu.Handle,
+                dstPitch = result_gpu.Pitch,
+                WidthInBytes = new IntPtr(sizeof(float) * source.Width),
+                Height = new IntPtr(source.Height)
+            };
+            return CUDAInterop.cuMemcpy2D(ptSt) == CUDAInterop.cudaError_enum.CUDA_SUCCESS
+                ? result_gpu
+                : throw new InvalidOperationException("Failed to copy the source data on the target GPU device");
+        }
 
-#else
-        private const string CUDA_DLL_NAME = "nvcuda";
-#endif
+        /// <summary>
+        /// Copies the contents of the input <see cref="DeviceMemory2D{T}"/> instance to the target host memory area
+        /// </summary>
+        /// <param name="source">The <see cref="DeviceMemory2D{T}"/> area to read</param>
+        /// <param name="destination">The destination <see cref="FloatSpan2D"/> memory to write on</param>
+        public static unsafe void CopyTo([NotNull] this DeviceMemory2D<float> source, in FloatSpan2D destination)
+        {
+            CUDAInterop.CUDA_MEMCPY2D_st* ptSt = stackalloc CUDAInterop.CUDA_MEMCPY2D_st[1];
+            ptSt[0] = new CUDAInterop.CUDA_MEMCPY2D_st
+            {
+                srcMemoryType = CUDAInterop.CUmemorytype_enum.CU_MEMORYTYPE_DEVICE,
+                srcDevice = source.Handle,
+                srcPitch = source.Pitch,
+                dstMemoryType = CUDAInterop.CUmemorytype_enum.CU_MEMORYTYPE_HOST,
+                dstHost = destination.Ptr,
+                dstPitch = new IntPtr(sizeof(float) * destination.Width),
+                WidthInBytes = new IntPtr(sizeof(float) * destination.Width),
+                Height = new IntPtr(destination.Height)
+            };
+            if (CUDAInterop.cuMemcpy2D(ptSt) != CUDAInterop.cudaError_enum.CUDA_SUCCESS)
+                throw new InvalidOperationException("Failed to copy the source data on the given destination");
+        }
+
+        /// <summary>
+        /// Copies the contents of the input <see cref="DeviceMemory2D{T}"/> to a new memory area on the unmanaged heap
+        /// </summary>
+        /// <param name="source">The source <see cref="DeviceMemory2D{T}"/> memory to copy</param>
+        /// <param name="result">The resulting maatrix</param>
+        [MustUseReturnValue]
+        public static void CopyToHost([NotNull] this DeviceMemory2D<float> source, out FloatSpan2D result)
+        {
+            MemoryLayout.Layout2D layout = source.Layout.To<MemoryLayout, MemoryLayout.Layout2D>();
+            FloatSpan2D.New(layout.height.ToInt32(), layout.width.ToInt32(), out result);
+            source.CopyTo(result);
+        }
 
         /// <summary>
         /// Gets the amount of available GPU memory for a given GPU
         /// </summary>
         /// <param name="gpu">The target <see cref="Gpu"/> to use to retrieve the info</param>
-        [PublicAPI]
-        public static ulong GetFreeMemory([NotNull] this Gpu gpu)
+        [Pure]
+        public static unsafe (ulong Free, ulong Total) GetFreeMemory([NotNull] this Gpu gpu)
         {
             // Set the context
-            int result = CUDA_SetContext(Gpu.Default.Context.Handle);
-            if (result != 0) throw new InvalidOperationException($"Error setting the GPU context: {result}");
+            CUDAInterop.cudaError_enum result = CUDAInterop.cuCtxSetCurrent(Gpu.Default.Context.Handle);
+            if (result != CUDAInterop.cudaError_enum.CUDA_SUCCESS) throw new InvalidOperationException($"Error setting the GPU context: {result}");
 
             // Get the memory info
-            IntPtr
-                free = new IntPtr(0),
-                total = new IntPtr(0);
-            result = CUDA_GetMemInfo(ref free, ref total);
+            IntPtr* pointers = stackalloc IntPtr[2];
+            result = CUDAInterop.cuMemGetInfo(pointers, pointers + 1);
             if (result != 0) throw new InvalidOperationException("Error while retrieving the memory info");
-            return (ulong)free.ToInt64();
+            return ((ulong)pointers[0].ToInt64(), (ulong)pointers[1].ToInt64());
         }
-
-        // Gets the info on the amount of free and total GPU memory available
-        [DllImport(CUDA_DLL_NAME, EntryPoint = "cuMemGetInfo_v2")]
-        private static extern int CUDA_GetMemInfo(ref IntPtr free, ref IntPtr total);
-
-        // Sets the GPU context for subsequent calls to the CUDA library
-        [DllImport(CUDA_DLL_NAME, EntryPoint = "cuCtxSetCurrent")]
-        public static extern int CUDA_SetContext(IntPtr pctx);
     }
 }
