98d9a0d965dcbf7321412f789e5fa8e794a7d7dc
Sergio Pedri <Sergio0694@live.com>
2017-12-05 17:04:11 +0100

    Added sample run automatic logging

--- Samples/DigitsCudaTest/Program.cs
+++ Samples/DigitsCudaTest/Program.cs
@@ -24,32 +24,31 @@ namespace DigitsCudaTest
             (var training, var test) = DataParser.LoadDatasets();
             INeuralNetwork network = NetworkManager.NewNetwork(
                 NetworkLayers.Convolutional((28, 28, 1), (5, 5), 10, ActivationFunctionType.Identity),
-                NetworkLayers.Pooling((24, 24, 10), ActivationFunctionType.Tanh),
-                NetworkLayers.FullyConnected(12 * 12 * 10, 100, ActivationFunctionType.Tanh),
+                NetworkLayers.Pooling((24, 24, 10), ActivationFunctionType.Sigmoid),
+                NetworkLayers.FullyConnected(12 * 12 * 10, 100, ActivationFunctionType.Sigmoid),
                 NetworkLayers.Softmax(100, 10));
 
             // Setup and start the training
             CancellationTokenSource cts = new CancellationTokenSource();
             Console.CancelKeyPress += (s, e) => cts.Cancel();
-            TrainingSessionResult result = await NetworkManager.TrainNetworkAsync(network, (training.X, training.Y), 4, 400, null,
+            TrainingSessionResult result = await NetworkManager.TrainNetworkAsync(network, (training.X, training.Y), 60, 400, null,
                 new TestParameters(test, new Progress<BackpropagationProgressEventArgs>(p =>
                 {
                     Printf($"Epoch {p.Iteration}, cost: {p.Result.Cost}, accuracy: {p.Result.Accuracy}");
-                })), 0.1f, 0.5f, 1, cts.Token);
+                })), 0.1f, 0.5f, token: cts.Token);
 
             // Save the training reports
             Printf($"Stop reason: {result.StopReason}, elapsed time: {result.TrainingTime}");
             String
-                code = Assembly.GetExecutingAssembly().Location,
-                dll = Path.GetFullPath(code),
-                root = Path.GetDirectoryName(dll),
+                root = Path.GetDirectoryName(Path.GetFullPath(Assembly.GetExecutingAssembly().Location)),
                 path = Path.Combine(root ?? throw new InvalidOperationException("The dll path can't be null"), "TrainingResults", DateTime.Now.ToString("yy-MM-dd"));
             Directory.CreateDirectory(path);
-            File.WriteAllText(Path.Combine(path, $"{DateTime.Now:yy-MM-dd}_cost.py"), result.TestReports.AsPythonMatplotlibChart(TrainingReportType.Cost));
-            File.WriteAllText(Path.Combine(path, $"{DateTime.Now:yy-MM-dd}_accuracy.py"), result.TestReports.AsPythonMatplotlibChart(TrainingReportType.Accuracy));
-            network.Save(new DirectoryInfo(path), DateTime.Now.ToString("yy-MM-dd"));
-            File.WriteAllText(Path.Combine(path, $"{DateTime.Now:yy-MM-dd}.json"), network.SerializeAsJson());
-            File.WriteAllText(Path.Combine(path, $"{DateTime.Now:yy-MM-dd}_report.json"), result.SerializeAsJson());
+            String timestamp = DateTime.Now.ToString("yy-MM-dd-mm-ss");
+            File.WriteAllText(Path.Combine(path, $"{timestamp}_cost.py"), result.TestReports.AsPythonMatplotlibChart(TrainingReportType.Cost));
+            File.WriteAllText(Path.Combine(path, $"{timestamp}_accuracy.py"), result.TestReports.AsPythonMatplotlibChart(TrainingReportType.Accuracy));
+            network.Save(new DirectoryInfo(path), timestamp);
+            File.WriteAllText(Path.Combine(path, $"{timestamp}.json"), network.SerializeAsJson());
+            File.WriteAllText(Path.Combine(path, $"{timestamp}_report.json"), result.SerializeAsJson());
             Console.ReadKey();
         }
 
