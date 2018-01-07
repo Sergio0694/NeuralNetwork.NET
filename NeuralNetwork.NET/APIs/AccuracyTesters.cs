4ecf9ef3fc96383fe2fdf14166a47c49749afe8a
Sergio0694 <Sergio0694@live.com>
2018-01-07 19:54:31 +0100

    Added the threshold accuracy tester option

--- NeuralNetwork.NET/APIs/AccuracyTesters.cs
+++ NeuralNetwork.NET/APIs/AccuracyTesters.cs
@@ -1,4 +1,5 @@
-﻿using JetBrains.Annotations;
+﻿using System;
+using JetBrains.Annotations;
 using NeuralNetworkNET.APIs.Delegates;
 using NeuralNetworkNET.Extensions;
 
@@ -15,5 +16,16 @@ namespace NeuralNetworkNET.APIs
         [PublicAPI]
         [Pure, NotNull]
         public static AccuracyTester Argmax() => (yHat, y) => yHat.Argmax(float.MinValue) == y.Argmax(float.MinValue);
+
+        /// <summary>
+        /// Gets an <see cref="AccuracyTester"/> <see langword="delegate"/> that can be used classification problems with mutually-exclusive classes
+        /// </summary>
+        [PublicAPI]
+        [Pure, NotNull]
+        public static AccuracyTester Threshold(float threshold = 0.5f)
+        {
+            if (threshold <= 0 || threshold >= 1) throw new ArgumentOutOfRangeException(nameof(threshold), "The threshold must be in the (0,1) range");
+            return (yHat, y) => yHat.MatchElementwiseThreshold(y, threshold);
+        }
     }
 }
