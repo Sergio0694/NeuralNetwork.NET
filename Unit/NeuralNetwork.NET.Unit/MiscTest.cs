8ffd619c6d2e1be4f020bdc05e8ceb09b93c01f6
Sergio0694 <Sergio0694@live.com>
2018-01-07 20:02:08 +0100

    Added a Unit test for the threshold tester

--- Unit/NeuralNetwork.NET.Unit/MiscTest.cs
+++ Unit/NeuralNetwork.NET.Unit/MiscTest.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NeuralNetworkNET.APIs;
+using NeuralNetworkNET.APIs.Delegates;
 using NeuralNetworkNET.Extensions;
 
 namespace NeuralNetworkNET.Unit
@@ -38,6 +39,18 @@ namespace NeuralNetworkNET.Unit
         public void CudaSupport()
         {
             Assert.IsFalse(CuDnnNetworkLayers.IsCudaSupportAvailable);
+        }
+
+        [TestMethod]
+        public void ThresholdAccuracyTest()
+        {
+            AccuracyTester tester = AccuracyTesters.Threshold();
+            Span<float>
+                yHat = new[] { 0.1f, 0.4f, 0.6f, 0.99f, 0.1f, 0.73f },
+                y1 = new[] { 0.8f, 0.1f, 0.8f, 0.3f, 0.2f, 0.66f },
+                y2 = new[] { 0.5f, 0.3f, 0.8f, 1, 0.004f, 0.990f };
+            Assert.IsFalse(tester(yHat, y1));
+            Assert.IsTrue(tester(yHat, y2));
         }
     }
 }
