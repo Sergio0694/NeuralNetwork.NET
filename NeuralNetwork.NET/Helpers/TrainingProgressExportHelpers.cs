5fe16618e3d609b214805377bcc2d26ce7570af0
Sergio0694 <Sergio0694@live.com>
2018-01-07 02:17:14 +0100

    Python chart code improved

--- NeuralNetwork.NET/Helpers/TrainingProgressExportHelpers.cs
+++ NeuralNetwork.NET/Helpers/TrainingProgressExportHelpers.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
-using System.IO;
 using System.Linq;
-using System.Reflection;
 using JetBrains.Annotations;
 using NeuralNetworkNET.APIs.Enums;
 using NeuralNetworkNET.APIs.Results;
@@ -15,6 +13,18 @@ namespace NeuralNetworkNET.Helpers
     /// </summary>
     public static class TrainingProgressExportHelpers
     {
+        // The Python matplotlib template
+        private static readonly String PyTemplate = @"import matplotlib.pyplot as plt
+                                                      x = [$VALUES$]
+                                                      plt.grid(linestyle=""dashed"")
+                                                      plt.ylabel(""$YLABEL$"")
+                                                      plt.xlabel(""Epoch"")
+                                                      plt.plot(x)
+                                                      plt.show()".Split(new[] { Environment.NewLine }, StringSplitOptions.None).Aggregate(String.Empty, (s, l) => $"{s}{l.Trim()}{Environment.NewLine}");
+
+        // The custom 4-spaces indentation for the data points (the \t character is not consistent across different editors)
+        private const String Tab = "    ";
+
         /// <summary>
         /// Returns a Python script to plot a 2D chart from the given progress reports
         /// </summary>
@@ -37,15 +47,13 @@ namespace NeuralNetworkNET.Helpers
             String
                 values = results
                     .Select((r, i) => (Result: r, Index: i))
-                    .Aggregate(String.Empty, (b, v) =>
+                    .Aggregate($"{Environment.NewLine}{Tab}", (b, v) =>
                     {
-                        String separator = v.Index == results.Count - 1 ? String.Empty : ",\n    ";
+                        String separator = v.Index == results.Count - 1 ? Environment.NewLine : $",{Environment.NewLine}{Tab}";
                         return $"{b}{GetResultValue(v.Result).ToString(CultureInfo.InvariantCulture)}{separator}";
                     }),
-                ylabel = type == TrainingReportType.Accuracy ? "Accuracy" : "Cost",
-                path = Path.Combine(Path.GetDirectoryName(Path.GetFullPath(Assembly.GetExecutingAssembly().Location)), "Assets", "PltTemplate.py"),
-                template = File.ReadAllText(path);
-            return template.Replace("$VALUES$", values).Replace("$YLABEL$", ylabel);
+                ylabel = type == TrainingReportType.Accuracy ? "Accuracy" : "Cost";
+            return PyTemplate.Replace("$VALUES$", values).Replace("$YLABEL$", ylabel);
         }
     }
 }
