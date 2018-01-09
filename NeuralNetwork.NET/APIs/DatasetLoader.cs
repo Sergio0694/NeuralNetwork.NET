60743bcfb070f8e669c07c87b05546e7515f0eb5
Sergio0694 <Sergio0694@live.com>
2018-01-09 02:04:53 +0100

    Added public APIs to load image datasets

--- NeuralNetwork.NET/APIs/DatasetLoader.cs
+++ NeuralNetwork.NET/APIs/DatasetLoader.cs
@@ -4,9 +4,12 @@ using System.Linq;
 using JetBrains.Annotations;
 using NeuralNetworkNET.APIs.Interfaces.Data;
 using NeuralNetworkNET.Extensions;
+using NeuralNetworkNET.Helpers;
 using NeuralNetworkNET.SupervisedLearning.Data;
 using NeuralNetworkNET.SupervisedLearning.Optimization.Parameters;
 using NeuralNetworkNET.SupervisedLearning.Optimization.Progress;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
 
 namespace NeuralNetworkNET.APIs
 {
@@ -35,7 +38,7 @@ namespace NeuralNetworkNET.APIs
         [PublicAPI]
         [Pure, NotNull]
         [CollectionAccess(CollectionAccessType.Read)]
-        public static ITrainingDataset Training([NotNull] IEnumerable<Func<(float[] X, float[] Y)>> data, int size) => BatchesCollection.From(data, size);
+        public static ITrainingDataset Training([NotNull, ItemNotNull] IEnumerable<Func<(float[] X, float[] Y)>> data, int size) => BatchesCollection.From(data, size);
 
         /// <summary>
         /// Creates a new <see cref="ITrainingDataset"/> instance to train a network from the input matrices, with the specified batch size
@@ -46,6 +49,17 @@ namespace NeuralNetworkNET.APIs
         [Pure, NotNull]
         [CollectionAccess(CollectionAccessType.Read)]
         public static ITrainingDataset Training((float[,] X, float[,] Y) data, int size) => BatchesCollection.From(data, size);
+
+        /// <summary>
+        /// Creates a new <see cref="ITrainingDataset"/> instance to train a network from the input data, where each input sample is an image in a specified format
+        /// </summary>
+        /// <typeparam name="TPixel">The type of image pixels. It must be either <see cref="Alpha8"/>, <see cref="Rgb24"/> or <see cref="Argb32"/></typeparam>
+        /// <param name="data">A list of <see cref="ValueTuple{T1, T2}"/> items, where the first element is the image path and the second is a vector with the expected outputs</param>
+        /// <param name="size">The desired dataset batch size</param>
+        /// <param name="modify">An optional <see cref="Action{T}"/> to modify each sample image when loading the dataset</param>
+        public static ITrainingDataset Training<TPixel>([NotNull] IEnumerable<(String X, float[] Y)> data, int size, [CanBeNull] Action<IImageProcessingContext<TPixel>> modify = null)
+            where TPixel : struct, IPixel<TPixel>
+            => BatchesCollection.From(data.Select<(String X, float[] Y), Func<(float[], float[])>>(xy => () => (ImageLoader.LoadSample(xy.X, modify), xy.Y)), size);
 
         #endregion
 
@@ -72,7 +86,7 @@ namespace NeuralNetworkNET.APIs
         [PublicAPI]
         [Pure, NotNull]
         [CollectionAccess(CollectionAccessType.Read)]
-        public static IValidationDataset Validation([NotNull] IEnumerable<Func<(float[] X, float[] Y)>> data, float tolerance = 1e-2f, int epochs = 5)
+        public static IValidationDataset Validation([NotNull, ItemNotNull] IEnumerable<Func<(float[] X, float[] Y)>> data, float tolerance = 1e-2f, int epochs = 5)
             => Validation(data.AsParallel().Select(f => f()), tolerance, epochs);
 
         /// <summary>
@@ -85,6 +99,18 @@ namespace NeuralNetworkNET.APIs
         [Pure, NotNull]
         [CollectionAccess(CollectionAccessType.Read)]
         public static IValidationDataset Validation((float[,] X, float[,] Y) data, float tolerance = 1e-2f, int epochs = 5) => new ValidationDataset(data, tolerance, epochs);
+
+        /// <summary>
+        /// Creates a new <see cref="IValidationDataset"/> instance to validate a network accuracy from the input collection
+        /// </summary>
+        /// <typeparam name="TPixel">The type of image pixels. It must be either <see cref="Alpha8"/>, <see cref="Rgb24"/> or <see cref="Argb32"/></typeparam>
+        /// <param name="data">A list of <see cref="ValueTuple{T1, T2}"/> items, where the first element is the image path and the second is a vector with the expected outputs</param>
+        /// <param name="tolerance">The desired tolerance to test the network for convergence</param>
+        /// <param name="epochs">The epochs interval to consider when testing the network for convergence</param>
+        /// <param name="modify">An optional <see cref="Action{T}"/> to modify each sample image when loading the dataset</param>
+        public static IValidationDataset Validation<TPixel>([NotNull] IEnumerable<(String X, float[] Y)> data, float tolerance = 1e-2f, int epochs = 5, [CanBeNull] Action<IImageProcessingContext<TPixel>> modify = null)
+            where TPixel : struct, IPixel<TPixel>
+            => Validation(data.Select<(String X, float[] Y), Func<(float[], float[])>>(xy => () => (ImageLoader.LoadSample(xy.X, modify), xy.Y)).AsParallel(), tolerance, epochs);
 
         #endregion
 
@@ -109,7 +135,7 @@ namespace NeuralNetworkNET.APIs
         [PublicAPI]
         [Pure, NotNull]
         [CollectionAccess(CollectionAccessType.Read)]
-        public static ITestDataset Test([NotNull] IEnumerable<Func<(float[] X, float[] Y)>> data, [CanBeNull] IProgress<TrainingProgressEventArgs> progress = null)
+        public static ITestDataset Test([NotNull, ItemNotNull] IEnumerable<Func<(float[] X, float[] Y)>> data, [CanBeNull] IProgress<TrainingProgressEventArgs> progress = null)
             => Test(data.AsParallel().Select(f => f()), progress);
 
         /// <summary>
@@ -121,6 +147,17 @@ namespace NeuralNetworkNET.APIs
         [Pure, NotNull]
         [CollectionAccess(CollectionAccessType.Read)]
         public static ITestDataset Test((float[,] X, float[,] Y) data, [CanBeNull] IProgress<TrainingProgressEventArgs> progress = null) => new TestDataset(data, progress);
+
+        /// <summary>
+        /// Creates a new <see cref="ITestDataset"/> instance to test a network from the input collection
+        /// </summary>
+        /// <typeparam name="TPixel">The type of image pixels. It must be either <see cref="Alpha8"/>, <see cref="Rgb24"/> or <see cref="Argb32"/></typeparam>
+        /// <param name="data">A list of <see cref="ValueTuple{T1, T2}"/> items, where the first element is the image path and the second is a vector with the expected outputs</param>
+        /// <param name="modify">An optional <see cref="Action{T}"/> to modify each sample image when loading the dataset</param>
+        /// <param name="progress">The optional progress callback to use</param>
+        public static ITestDataset Test<TPixel>([NotNull] IEnumerable<(String X, float[] Y)> data, [CanBeNull] IProgress<TrainingProgressEventArgs> progress = null, [CanBeNull] Action<IImageProcessingContext<TPixel>> modify = null)
+            where TPixel : struct, IPixel<TPixel>
+            => Test(data.Select<(String X, float[] Y), Func<(float[], float[])>>(xy => () => (ImageLoader.LoadSample(xy.X, modify), xy.Y)).AsParallel(), progress);
 
         #endregion
     }
