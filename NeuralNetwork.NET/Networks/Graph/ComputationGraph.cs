714bf9361ba818af0f0c5c3e28846c3f9d424ef8
Sergio0694 <Sergio0694@live.com>
2018-01-14 18:58:54 +0100

    Added initial graph structure implementation

---
+++ NeuralNetwork.NET/Networks/Graph/ComputationGraph.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using JetBrains.Annotations;
+using NeuralNetworkNET.APIs.Interfaces;
+
+namespace NeuralNetworkNET.Networks.Graph
+{
+    /// <summary>
+    /// A graph of <see cref="INetworkLayer"/> instances, with O(1) pre-order access time for nodes
+    /// </summary>
+    public sealed class ComputationGraph : IReadOnlyList<INetworkLayer>
+    {
+        /// <summary>
+        /// Gets the root <see cref="ComputationGraphNode"/> for the current graph
+        /// </summary>
+        [NotNull]
+        public ComputationGraphNode Root { get; }
+
+        // The in-order serialized view of the network layers in the graph
+        [NotNull, ItemNotNull]
+        private readonly IReadOnlyList<INetworkLayer> Layers;
+        
+        internal ComputationGraph(ComputationGraphNode root)
+        {
+            Root = root;
+
+            // Explore the graph and build the serialized list
+            List<INetworkLayer> layers = new List<INetworkLayer>();
+            void Explore(ComputationGraphNode node)
+            {
+                layers.Add(node.Layer);
+                foreach (ComputationGraphNode child in node.Children)
+                    Explore(child);
+            }
+            Explore(root);
+            Layers = layers;
+        }
+
+        #region Interface
+
+        /// <inheritdoc/>
+        public IEnumerator<INetworkLayer> GetEnumerator() => Layers.GetEnumerator();
+
+        /// <inheritdoc/>
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        /// <inheritdoc/>
+        public int Count
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => Layers.Count;
+        }
+
+        /// <inheritdoc/>
+        public INetworkLayer this[int index]
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => Layers[index];
+        }
+
+        #endregion
+    }
+}
---
+++ NeuralNetwork.NET/Networks/Graph/ComputationGraphNode.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using JetBrains.Annotations;
+using NeuralNetworkNET.APIs.Interfaces;
+
+namespace NeuralNetworkNET.Networks.Graph
+{
+    /// <summary>
+    /// A class representing a single node in a computation graph
+    /// </summary>
+    public sealed class ComputationGraphNode
+    {
+        /// <summary>
+        /// Gets the layer associated with the current graph node
+        /// </summary>
+        [NotNull]
+        public INetworkLayer Layer { get; }
+        
+        /// <summary>
+        /// Gets the parent node for the current graph node
+        /// </summary>
+        [NotNull]
+        public ComputationGraphNode Parent { get; }
+
+        /// <summary>
+        /// Gets the collection of children nodes for the current node
+        /// </summary>
+        [NotNull, ItemNotNull]
+        public IReadOnlyList<ComputationGraphNode> Children { get; }
+
+        internal ComputationGraphNode([NotNull] INetworkLayer layer, [NotNull] ComputationGraphNode parent, [NotNull, ItemNotNull] IReadOnlyList<ComputationGraphNode> children)
+        {
+            Layer = layer;
+            Parent = parent;
+            Children = children;
+        }
+    }
+}
