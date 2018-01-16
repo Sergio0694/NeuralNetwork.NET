0f788035c07790900f09c62771e465d87ec1dd42
Sergio0694 <Sergio0694@live.com>
2018-01-16 17:50:49 +0100

    ComputationGraph initialization improved

--- NeuralNetwork.NET/Networks/Graph/ComputationGraph.cs
+++ NeuralNetwork.NET/Networks/Graph/ComputationGraph.cs
@@ -1,6 +1,6 @@
-﻿using System.Collections;
+﻿using System;
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
+using System.Linq;
 using JetBrains.Annotations;
 using NeuralNetworkNET.APIs.Interfaces;
 
@@ -9,43 +9,62 @@ namespace NeuralNetworkNET.Networks.Graph
     /// <summary>
     /// A graph of <see cref="INetworkLayer"/> instances, with O(1) pre-order access time for nodes
     /// </summary>
-    public sealed class ComputationGraph : IReadOnlyList<INetworkLayer>
+    public sealed class ComputationGraph
     {
         /// <summary>
-        /// Gets the root <see cref="ProcessingNode"/> for the current graph
+        /// Gets the root <see cref="IComputationGraphNode"/> for the current graph
         /// </summary>
         [NotNull]
-        public ProcessingNode Root { get; }
+        public IComputationGraphNode Root { get; }
 
-        // The in-order serialized view of the network layers in the graph
+        /// <summary>
+        /// Gets the in-order serialized view of the network graph nodes
+        /// </summary>
         [NotNull, ItemNotNull]
-        private readonly IReadOnlyList<INetworkLayer> Layers;
+        internal readonly IReadOnlyList<IComputationGraphNode> Layers;
         
-        internal ComputationGraph(ProcessingNode root)
+        internal ComputationGraph(IComputationGraphNode root)
         {
             Root = root;
+            Layers = TrySerializeLayers(root) ?? throw new ArgumentException("The input graph isn't valid", nameof(root));
         }
 
-        #region Interface
+        #region Tools
 
-        /// <inheritdoc/>
-        public IEnumerator<INetworkLayer> GetEnumerator() => Layers.GetEnumerator();
-
-        /// <inheritdoc/>
-        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
-
-        /// <inheritdoc/>
-        public int Count
+        /// <summary>
+        /// Tries to serialize the input graph into a sequence of <see cref="IComputationGraphNode"/> instances, if possible
+        /// </summary>
+        /// <param name="root">The root <see cref="IComputationGraphNode"/> for the computation graph</param>
+        [Pure, CanBeNull]
+        private static IReadOnlyList<IComputationGraphNode> TrySerializeLayers(IComputationGraphNode root)
         {
-            [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            get => Layers.Count;
-        }
+            HashSet<IComputationGraphNode> nodes = new HashSet<IComputationGraphNode>();
 
-        /// <inheritdoc/>
-        public INetworkLayer this[int index]
-        {
-            [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            get => Layers[index];
+            bool Explore(IComputationGraphNode node)
+            {
+                // Add the current node, if not existing
+                if (nodes.Contains(node)) return false;
+                nodes.Add(node);
+
+                // Explore the graph
+                switch (node)
+                {
+                    case ProcessingNode processing:
+                        if (!processing.Children.All(Explore)) return false;
+                        break;
+                    case MergeNode merge:
+                        if (!merge.Children.All(Explore)) return false;
+                        break;
+                    case TrainingSplitNode split:
+                        if (!Explore(split.InferenceBranchNode)) return false;
+                        if (!Explore(split.TrainingBranchNode)) return false;
+                        break;
+                    default: throw new ArgumentException("Invalid node type", nameof(node));
+                }
+                return true;
+            }
+
+            return Explore(root) ? nodes.ToArray() : null;
         }
 
         #endregion
