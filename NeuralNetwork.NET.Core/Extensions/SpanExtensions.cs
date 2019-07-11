0af69eedaa7aa874fbb59a48dc9fb4cb3d58be83
Sergio Pedri <sergio0694@live.com>
2019-07-11 16:35:58 +0200

    Added SpanExtensions class

---
+++ NeuralNetwork.NET.Core/Extensions/SpanExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using JetBrains.Annotations;
+
+namespace NeuralNetwork.NET.Core.Extensions
+{
+    /// <summary>
+    /// An helper class with methods to process fixed-size matrices
+    /// </summary>
+    public static class SpanExtensions
+    {
+        /// <summary>
+        /// Returns an hash code for the contents of the input <see cref="Span{T}"/>
+        /// </summary>
+        /// <typeparam name="T">The type of each value in the input <see cref="Span{T}"/></typeparam>
+        /// <param name="span">The input <see cref="Span{T}"/> to read</param>
+        [Pure]
+        public static int GetContentHashCode<T>(this Span<T> span) where T : unmanaged
+        {
+            int hash = 17;
+            unchecked
+            {
+                for (int i = 0; i < span.Length; i++)
+                    hash = hash * 23 + span[i].GetHashCode();
+                return hash;
+            }
+        }
+    }
+}
