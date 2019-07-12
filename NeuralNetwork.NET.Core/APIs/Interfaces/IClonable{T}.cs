7f1964d821ee1d3145ceedabc039f8619011b238
Sergio Pedri <sergio0694@live.com>
2019-07-12 15:48:18 +0200

    Added ILayer interface

--- NeuralNetwork.NET.Core/APIs/Interfaces/IClonable{T}.cs
+++ NeuralNetwork.NET.Core/APIs/Interfaces/IClonable{T}.cs
@@ -1,6 +1,6 @@
 ﻿using JetBrains.Annotations;
 
-namespace NeuralNetworkDotNet.Core.Interfaces
+namespace NeuralNetworkDotNet.Core.APIs.Interfaces
 {
     /// <summary>
     /// An interface for an object that supports deep cloning
---
+++ NeuralNetwork.NET.Core/APIs/Interfaces/ILayer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace NeuralNetworkDotNet.Core.APIs.Interfaces
+{
+    /// <summary>
+    /// An interface that represents a single layer in a network
+    /// </summary>
+    public interface ILayer : IEquatable<ILayer>, IClonable<ILayer>
+    {
+        /// <summary>
+        /// Gets the shape of the layer inputs
+        /// </summary>
+        (int C, int H, int W) InputShape { get; }
+
+        /// <summary>
+        /// Gets the shape of the layer outputs
+        /// </summary>
+        (int C, int H, int W) OutputShape { get; }
+    }
+}
