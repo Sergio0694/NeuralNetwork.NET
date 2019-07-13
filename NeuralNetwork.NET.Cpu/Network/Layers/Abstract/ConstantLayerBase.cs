2ab929ca1a65e034b3f6c98a3addeb2c2ec48ee0
Sergio Pedri <sergio0694@live.com>
2019-07-13 16:32:55 +0200

    Added constant layer base

---
+++ NeuralNetwork.NET.Cpu/Network/Layers/Abstract/ConstantLayerBase.cs
@@ -0,0 +1,23 @@
+﻿using JetBrains.Annotations;
+using NeuralNetworkDotNet.APIs.Models;
+using NeuralNetworkDotNet.APIs.Structs;
+using NeuralNetworkDotNet.Network.Layers.Abstract.Base;
+
+namespace NeuralNetworkDotNet.Network.Layers.Abstract
+{
+    /// <summary>
+    /// A base <see langword="class"/> for a network layer with no parameters to optimize during training
+    /// </summary>
+    internal abstract class ConstantLayerBase : LayerBase
+    {
+        protected ConstantLayerBase(Shape input, Shape output) : base(input, output) { }
+
+        /// <summary>
+        /// Backpropagates the error to compute the delta for the inputs of the layer
+        /// </summary>
+        /// <param name="x">The layer inputs used in the forward pass</param>
+        /// <param name="y">The output <see cref="Tensor"/> computed in the forward pass</param>
+        /// <param name="dy">The output error delta to backpropagate</param>
+        public abstract Tensor Backpropagate([NotNull] Tensor x, [NotNull] Tensor y, [NotNull] Tensor dy);
+    }
+}
