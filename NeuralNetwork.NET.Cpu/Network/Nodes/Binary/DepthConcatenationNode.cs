c64461573c2726c0591cd8848fb901cf54c7980e
Sergio Pedri <sergio0694@live.com>
2019-07-16 21:29:46 +0200

    Added depth concatenation node

---
+++ NeuralNetwork.NET.Cpu/Network/Nodes/Binary/DepthConcatenationNode.cs
@@ -0,0 +1,32 @@
+﻿using JetBrains.Annotations;
+using NeuralNetworkDotNet.APIs.Models;
+using NeuralNetworkDotNet.cpuDNN;
+using NeuralNetworkDotNet.Network.Nodes.Abstract;
+
+namespace NeuralNetworkDotNet.Network.Nodes.Binary
+{
+    /// <summary>
+    /// A depth concatenation node, to merge multiple input nodes together
+    /// </summary>
+    internal sealed class DepthConcatenationNode : BinaryNodeBase
+    {
+        public DepthConcatenationNode([NotNull] INode left, [NotNull] INode right)
+            : base(left, right, (left.Shape.C + right.Shape.C, left.Shape.H, left.Shape.W))
+        { }
+
+        /// <inheritdoc/>
+        public override Tensor Forward(Tensor x1, Tensor x2)
+        {
+            var y = Tensor.New(x1.Shape.N, Shape.C, Shape.H, Shape.W);
+            CpuDnn.DepthConcatenationForward(x1, x2, y);
+
+            return y;
+        }
+
+        /// <inheritdoc/>
+        public override Tensor Backward(Tensor x1, Tensor x2, Tensor y, Tensor dy)
+        {
+            return dy.Clone();
+        }
+    }
+}
