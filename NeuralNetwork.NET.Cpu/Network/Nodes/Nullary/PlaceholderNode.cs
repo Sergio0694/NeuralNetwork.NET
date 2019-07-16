f6fed5d27da127493687b118210807c818f8c152
Sergio Pedri <sergio0694@live.com>
2019-07-17 00:29:28 +0200

    More code refactoring

---
+++ NeuralNetwork.NET.Cpu/Network/Nodes/Nullary/PlaceholderNode.cs
@@ -0,0 +1,12 @@
+﻿using NeuralNetworkDotNet.APIs.Structs;
+
+namespace NeuralNetworkDotNet.Network.Nodes.Nullary
+{
+    /// <summary>
+    /// A placeholder node used for the inputs to a computational graph
+    /// </summary>
+    internal sealed class PlaceholderNode : Node
+    {
+        public PlaceholderNode(Shape shape) : base(shape) { }
+    }
+}
