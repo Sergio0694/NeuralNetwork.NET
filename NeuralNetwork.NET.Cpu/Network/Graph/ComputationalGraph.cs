e5e0ff58d46602a5ba81e91f40221cabe5a6e006
Sergio Pedri <sergio0694@live.com>
2019-07-17 19:40:39 +0200

    Implemented forward pass in the graph network

--- NeuralNetwork.NET.Cpu/Network/Graph/ComputationalGraph.cs
+++ NeuralNetwork.NET.Cpu/Network/Graph/ComputationalGraph.cs
@@ -6,7 +6,9 @@ using JetBrains.Annotations;
 using NeuralNetworkDotNet.APIs.Interfaces;
 using NeuralNetworkDotNet.APIs.Models;
 using NeuralNetworkDotNet.APIs.Structs;
+using NeuralNetworkDotNet.Extensions.Types;
 using NeuralNetworkDotNet.Network.Nodes;
+using NeuralNetworkDotNet.Network.Nodes.Abstract;
 using NeuralNetworkDotNet.Network.Nodes.Unary.Abstract;
 
 namespace NeuralNetworkDotNet.Network.Graph
@@ -40,14 +42,54 @@ namespace NeuralNetworkDotNet.Network.Graph
             throw new NotImplementedException();
         }
 
+        /// <inheritdoc/>
         public Tensor Forward(Tensor x)
         {
-            throw new NotImplementedException();
+            using (var map = new DisposableDictionary<Node, Tensor> { [Input] = x })
+            {
+                // Recursive function to forward the inputs through the graph
+                void Forward(Node node)
+                {
+                    for (var i = 0; i < node.Children.Count; i++)
+                    {
+                        var child = node.Children[i];
+                        switch (child)
+                        {
+                            case UnaryNodeBase unary:
+                                map[unary] = unary.Forward(map[node]);
+                                break;
+                            case BinaryNodeBase binary:
+                                if (map.TryGetValue(binary.LeftParent, out var x1) &&
+                                    map.TryGetValue(binary.RightParent, out var x2))
+                                {
+                                    map[binary] = binary.Forward(x1, x2);
+                                }
+                                break;
+                            default: throw new InvalidOperationException("Invalid network graph");
+                        }
+
+                        Forward(child);
+                    }
+                }
+
+                // Forward the input tensor starting from the input node
+                Forward(Input);
+
+                // Get the output tensor and remove it from the mapping to keep it alive
+                var yHat = map[Output];
+                map.Remove(Output);
+
+                return yHat;
+            }
         }
 
+        /// <inheritdoc/>
         public float Loss(Tensor x, Tensor y)
         {
-            throw new NotImplementedException();
+            using (var yHat = Forward(x))
+            {
+                return Output.CostFunctions.Cost(yHat, y);
+            }
         }
 
         public string SerializeMetadataAsJson()
