a5742061e364c956531824ab6cb597459de89602
Sergio Pedri <sergio0694@live.com>
2019-07-23 01:04:15 +0200

    Added IEnvironment interface

---
+++ NeuralNetwork.NET/APIs/Interfaces/IEnvironment.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using JetBrains.Annotations;
+
+namespace NeuralNetworkNET.APIs.Interfaces
+{
+    /// <summary>
+    /// An <see langword="interface"/> for an environment to use to train a network using reinforced learning
+    /// </summary>
+    public interface IEnvironment : IClonable<IEnvironment>, IEquatable<IEnvironment>, IDisposable
+    {
+        /// <summary>
+        /// Gets the size of the current environment
+        /// </summary>
+        int Size { get; }
+
+        /// <summary>
+        /// Gets the number of available actions for the current environment
+        /// </summary>
+        int Actions { get; }
+
+        /// <summary>
+        /// Gets the reward for the current state of the environment
+        /// </summary>
+        int Reward { get; }
+
+        /// <summary>
+        /// Gets the timestep for the current environment
+        /// </summary>
+        int Timestep { get; }
+
+        /// <summary>
+        /// Executes an action with the specified index, and returns a new <see cref="IEnvironment"/> instance
+        /// </summary>
+        /// <param name="action">The index of the action to execute</param>
+        /// <returns>The new, updated <see cref="IEnvironment"/> instance</returns>
+        [Pure, NotNull]
+        IEnvironment Execute(int action);
+    }
+}
