306004e9c4a374c24efe891b3ec8c15120970ac6
Sergio Pedri <sergio0694@live.com>
2019-07-23 01:04:25 +0200

    Added tic-tac-toe environment

---
+++ NeuralNetwork.NET/ReinforcedLearning/Environments/TicTacToeEnvironment.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Buffers;
+using JetBrains.Annotations;
+using NeuralNetworkNET.APIs.Interfaces;
+using NeuralNetworkNET.Extensions;
+
+namespace NeuralNetworkNET.ReinforcedLearning.Environments
+{
+    /// <summary>
+    /// An environment for the tic-tac-toe game
+    /// </summary>
+    public sealed class TicTacToeEnvironment : IEnvironment
+    {
+        /// <inheritdoc/>
+        public int Size { get; } = 9;
+
+        /// <inheritdoc/>
+        public int Actions { get; } = 9;
+
+        /// <inheritdoc/>
+        public int Reward => Timestep;
+
+        /// <inheritdoc/>
+        public int Timestep { get; }
+
+        /// <summary>
+        /// A singleton <see cref="ArrayPool{T}"/> instance to quickly create new instances of the environment
+        /// </summary>
+        [NotNull]
+        private static readonly ArrayPool<float> Allocator = ArrayPool<float>.Create(9, 1000);
+
+        /// <summary>
+        /// The array representing the environment current state
+        /// </summary>
+        [NotNull]
+        private readonly float[] State = Allocator.Rent(9);
+
+        /// <summary>
+        /// Creates a new, empty <see cref="TicTacToeEnvironment"/> instance
+        /// </summary>
+        public TicTacToeEnvironment() { }
+
+        private TicTacToeEnvironment(int timestep) { Timestep = timestep; }
+
+        /// <inheritdoc/>
+        public IEnvironment Execute(int action)
+        {
+            bool valid = State[action].EqualsWithDelta(0);
+            var instance = new TicTacToeEnvironment(Timestep + 1);
+            if (valid) State[action] = Timestep % 2 == 0 ? 1 : -1;
+            return instance;
+        }
+
+        /// <inheritdoc/>
+        public IEnvironment Clone()
+        {
+            var instance = new TicTacToeEnvironment(Timestep);
+            State.AsSpan(0, Size).CopyTo(instance.State.AsSpan(0, Size));
+            return instance;
+        }
+
+        /// <inheritdoc/>
+        public bool Equals(IEnvironment other)
+        {
+            return other is TicTacToeEnvironment instance &&
+                   State.AsSpan(0, Size).ContentEquals(instance.State.AsSpan(0, Size));
+        }
+
+        /// <inheritdoc/>
+        ~TicTacToeEnvironment() => Allocator.Return(State);
+
+        /// <inheritdoc/>
+        public void Dispose()
+        {
+            Allocator.Return(State);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
