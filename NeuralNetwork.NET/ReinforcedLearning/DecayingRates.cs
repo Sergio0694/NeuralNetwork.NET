d2f6893d30d21f2eb5e7d1ab7696ee82297c709e
Sergio Pedri <sergio0694@live.com>
2019-07-27 15:10:06 +0200

    Added decaying rates

---
+++ NeuralNetwork.NET/ReinforcedLearning/DecayingRates.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+
+namespace NeuralNetworkNET.ReinforcedLearning
+{
+    /// <summary>
+    /// A static class that produces different decaying rates to be used during training
+    /// </summary>
+    public static class DecayingRates
+    {
+        /// <summary>
+        /// Creates a new exponential decaying enumerator
+        /// </summary>
+        /// <param name="decay">The decay factor to use</param>
+        [Pure, NotNull]
+        public static IEnumerator<float> Exponential(float decay)
+        {
+            var i = 0;
+            while (true)
+            {
+                yield return (float)Math.Exp(-i / decay);
+                i++;
+            }
+        }
+
+        /// <summary>
+        /// Creates a new linear decaying enumerator
+        /// </summary>
+        /// <param name="factor">The decaying factor to use</param>
+        [Pure, NotNull]
+        public static IEnumerator<float> Linear(float factor)
+        {
+            var value = 1f;
+            while (true)
+            {
+                yield return value;
+                value *= factor;
+            }
+        }
+    }
+}
